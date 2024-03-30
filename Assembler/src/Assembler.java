14c2ca9a90da09a16c3a2eb067218f4d94b697d4
Luai Waleed <mistaluai@gmail.com>
2024-03-31 00:02:15 +0200

    implemented the functions initializer

--- Assembler/src/Assembler.java
+++ Assembler/src/Assembler.java
@@ -2,46 +2,68 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Assembler {
- Map<String, Integer> InstructionOPcode;
+    Map<String, Integer> instructionOPcode;
+    Map<String, Integer> instructionFunction;
 
- Assembler() {
-     InitializeOpcodes();
- }
- private void InitializeOpcodes() {
-     InstructionOPcode = new HashMap<>();
+    Assembler() {
+        initializeOpcodes();
+        initializeFunctions();
+    }
 
-     //R-Type
-     InstructionOPcode.put("AND", 0);
-     InstructionOPcode.put("OR", 0);
-     InstructionOPcode.put("XOR", 0);
-     InstructionOPcode.put("NOR", 0);
+    private void initializeOpcodes() {
+        instructionOPcode = new HashMap<>();
 
-     InstructionOPcode.put("ADD", 1);
-     InstructionOPcode.put("SUB", 1);
-     InstructionOPcode.put("SLT", 1);
-     InstructionOPcode.put("SLTU", 1);
+        //R-Type
+        instructionOPcode.put("AND", 0);
+        instructionOPcode.put("OR", 0);
+        instructionOPcode.put("XOR", 0);
+        instructionOPcode.put("NOR", 0);
 
-     InstructionOPcode.put("JR", 2);
+        instructionOPcode.put("ADD", 1);
+        instructionOPcode.put("SUB", 1);
+        instructionOPcode.put("SLT", 1);
+        instructionOPcode.put("SLTU", 1);
 
-     //I-Type
-     InstructionOPcode.put("ANDI", 4);
-     InstructionOPcode.put("ORI", 5);
-     InstructionOPcode.put("XORI", 6);
-     InstructionOPcode.put("ADDI", 7);
-     InstructionOPcode.put("SLL", 8);
-     InstructionOPcode.put("SRL", 9);
-     InstructionOPcode.put("SRA", 10);
-     InstructionOPcode.put("ROR", 11);
-     InstructionOPcode.put("LW", 12);
-     InstructionOPcode.put("SW", 13);
-     InstructionOPcode.put("BEQ", 14);
-     InstructionOPcode.put("BNE", 15);
-     InstructionOPcode.put("BLT", 16);
-     InstructionOPcode.put("BGE", 17);
+        instructionOPcode.put("JR", 2);
 
-     //J-Type
-     InstructionOPcode.put("LUI", 18);
-     InstructionOPcode.put("J", 30);
-     InstructionOPcode.put("JAL", 31);
- }
+        //I-Type
+        instructionOPcode.put("ANDI", 4);
+        instructionOPcode.put("ORI", 5);
+        instructionOPcode.put("XORI", 6);
+        instructionOPcode.put("ADDI", 7);
+        instructionOPcode.put("SLL", 8);
+        instructionOPcode.put("SRL", 9);
+        instructionOPcode.put("SRA", 10);
+        instructionOPcode.put("ROR", 11);
+        instructionOPcode.put("LW", 12);
+        instructionOPcode.put("SW", 13);
+        instructionOPcode.put("BEQ", 14);
+        instructionOPcode.put("BNE", 15);
+        instructionOPcode.put("BLT", 16);
+        instructionOPcode.put("BGE", 17);
+
+        //J-Type
+        instructionOPcode.put("LUI", 18);
+        instructionOPcode.put("J", 30);
+        instructionOPcode.put("JAL", 31);
+    }
+
+    private void initializeFunctions() {
+        instructionFunction = new HashMap<>();
+
+        //Opcode 0
+        instructionFunction.put("AND", 0);
+        instructionFunction.put("OR", 1);
+        instructionFunction.put("XOR", 2);
+        instructionFunction.put("NOR", 3);
+
+        //Opcode 1
+        instructionFunction.put("ADD", 0);
+        instructionFunction.put("SUB", 1);
+        instructionFunction.put("SLT", 2);
+        instructionFunction.put("SLTU", 3);
+
+        //Opcode 2
+        instructionFunction.put("JR", 0);
+    }
 }
