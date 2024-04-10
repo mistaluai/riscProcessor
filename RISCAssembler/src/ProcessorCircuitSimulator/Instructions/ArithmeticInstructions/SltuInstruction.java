194513406033bf69e3f948e6de6e79210ecb95c0
Luai Waleed <mistaluai@gmail.com>
2024-04-11 01:28:14 +0200

    implemented slt and sltu instructions

---
+++ RISCAssembler/src/ProcessorCircuitSimulator/Instructions/ArithmeticInstructions/SltuInstruction.java
@@ -0,0 +1,28 @@
+package ProcessorCircuitSimulator.Instructions.ArithmeticInstructions;
+
+import ProcessorCircuitSimulator.DataPath.DataMemory;
+import ProcessorCircuitSimulator.DataPath.ProgramCounter;
+import ProcessorCircuitSimulator.DataPath.RegisterFile;
+import ProcessorCircuitSimulator.Instructions.Instruction;
+
+import static ProcessorCircuitSimulator.ALU.ArithmeticOperations.setLessThanUnsigned;
+
+public class SltuInstruction extends Instruction {
+    public SltuInstruction(RegisterFile registerFile, DataMemory memory, ProgramCounter programCounter, int[] parameters) {
+        super(registerFile, memory, programCounter, parameters);
+    }
+
+    public void execute() {
+
+        int rd = parameters[0];
+        int rs = parameters[1];
+        int rt = parameters[2];
+
+        String rsValue = registerFile.getRegister(rs);
+        String rtValue = registerFile.getRegister(rt);
+
+        String result = setLessThanUnsigned(rsValue, rtValue);
+
+        registerFile.setRegister(rd, result);
+    }
+}
